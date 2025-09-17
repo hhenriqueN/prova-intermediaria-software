1ebf7d5a819a86a0da57a11c0b47d6df74266d2e
hhenriqueN <heliohmsn@al.insper.edu.br>
2025-09-17 10:44:37 -0300

    Commit inicial

---
+++ src/test/java/com/example/prova_intermediaria_software/ProvaIntermediariaSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prova_intermediaria_software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaIntermediariaSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
